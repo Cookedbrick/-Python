09499c56cba7e8d75396d34f49e81eba087732d7
Cookedbrick <75474393+Cookedbrick@users.noreply.github.com>
2022-07-06 14:09:43 +0530

    Update 3-7.py

--- 3-7.py
+++ 3-7.py
@@ -1,9 +1,9 @@
-def three_seven():
+def three_seven(text):
     def my_func(word):
         return (chr(ord(word[0])-32) + word[1:])
     string = []
-    for i in input('Введите предложение\n').split():
+    for i in text.split():
         string.append(my_func(i))
     string = ' '.join(string)
     return (string)
-print(three_seven())
+print(three_seven('text for test'))
